4de44368f533d13c2db346cacbfa012acf84339b
TariqKalai <23047@ecam.be>
2025-11-15 22:55:44 +0100

    Added homepage menu, Stats and quickactions

---
+++ Chess DB/Controls/HomeMenu/Stats/QuickActions.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Chess_DB.Home_actions
+{
+    public partial class QuickActions : UserControl
+    {
+        public QuickActions()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ Chess DB/Controls/HomeMenu/Stats/Stats.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Chess_DB.Home_actions
+{
+    public partial class Stats : UserControl
+    {
+        public Stats()
+        {
+            InitializeComponent();
+        }
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
