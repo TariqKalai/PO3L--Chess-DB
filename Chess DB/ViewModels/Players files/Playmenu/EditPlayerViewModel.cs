cba0752b7e25b8598d6549aa27ab7e0b2c10d742
TariqKalai <23047@ecam.be>
2025-11-27 23:51:58 +0100

    Styling EditPlayer.axaml

--- Chess DB/ViewModels/Players files/Playmenu/EditPlayerViewModel.cs
+++ Chess DB/ViewModels/Players files/Playmenu/EditPlayerViewModel.cs
@@ -7,6 +7,7 @@ using Chess_DB.Services;
 
 public partial class EditPlayerViewModel : ViewModelBase
 {
+    // Just loadiing player in a variable to use it later in axaml
     public ObservableCollection<Player> List_player => AppServices.PlayerService.Players;
     public EditPlayerViewModel()
     {
